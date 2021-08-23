165f631b0214e10f3b76c9aad8b46ce5732fb8b8
zacinthepark <zacinthepark@gmail.com>
2021-08-23 18:37:46 +0900

    Added updateScreen and endCounting functions to ViewController file

--- Pomodoro/Pomodoro.swift
+++ Pomodoro/Pomodoro.swift
@@ -11,22 +11,24 @@ struct Pomodoro {
     
     var studyTime: Int
     var breakTime: Int
+    var totalTime: Int {
+        return studyTime + breakTime
+    }
     
     var pomodoros: Int {
-        let totalTime = studyTime + breakTime
         return totalTime / 30
     }
         
     func showResult() -> DailyResult {
-        if self.pomodoros >= 20 {
+        if pomodoros >= 20 {
             return .phenomenal
-        } else if self.pomodoros >= 16 {
+        } else if pomodoros >= 16 {
             return .verygood
-        } else if self.pomodoros >= 10 {
+        } else if pomodoros >= 10 {
             return .good
-        } else if self.pomodoros >= 6 {
+        } else if pomodoros >= 6 {
             return .bad
-        } else if self.pomodoros >= 2 {
+        } else if pomodoros >= 2 {
             return .verybad
         } else {
             return .terrible
--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -7,19 +7,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private weak var guideImageView: UIImageView!
+    @IBOutlet private weak var pomodoroLabel: UILabel!
+    @IBOutlet private weak var timeLabel: UILabel!
+    @IBOutlet private weak var studyButton: UIButton!
+    @IBOutlet private weak var breakButton: UIButton!
+    @IBOutlet private weak var endButton: UIButton!
+    @IBOutlet private weak var resetButton: UIButton!
+ 
+    private var dailyPomodoro = Pomodoro(studyTime: 0, breakTime: 0)
+    private var pomodoroEmoji = ""
+    
+}
+
+
+extension ViewController {
+    
+    private func updateScreen() {
+        let image = UIImage(named: "pomodoro")
+        guideImageView.image = image
+        
+        var pomodoroCount = dailyPomodoro.pomodoros
+        while pomodoroCount > 0 {
+            pomodoroEmoji += "🍅"
+            pomodoroCount = pomodoroCount - 1
+        }
+        pomodoroLabel.text = pomodoroEmoji
+        
+        timeLabel.isHidden = true
+    }
+}
+
+extension ViewController {
+    
+    private func endCounting() {
+        studyButton.isEnabled = false
+        breakButton.isEnabled = false
+        timeLabel.isHidden = false
+        timeLabel.text = "Daily Total: Studied \(dailyPomodoro.studyTime)mins with \(dailyPomodoro.breakTime)mins of break"
+    }
+    
+}
+
+extension ViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBOutlet weak var guideImageView: UIImageView!
-    @IBOutlet weak var pomodoroLabel: UILabel!
-    @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var studyButton: UIButton!
-    @IBOutlet weak var breakButton: UIButton!
-    @IBOutlet weak var endButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
     
 }
+
+extension ViewController {
+    
+    @IBAction private func tapStudyButton(_ sender: UIButton) {
+        dailyPomodoro.studyTime += 25
+    }
+    
+}
+
+extension ViewController {
+    
+    @IBAction private func tapBreakButton(_ sender: UIButton) {
+        dailyPomodoro.breakTime += 5
+    }
+    
+}
+
+extension ViewController {
+    
+    @IBAction private func tapEndButton(_ sender: UIButton) {
+    }
+    
+}
+
 
